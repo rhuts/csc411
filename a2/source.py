576dc4ec6d9bd948a18ab16b3ce7233cbf4b4840
rhuts <roman.h1234@gmail.com>
2019-11-03 20:48:06 -0500

    finish q2

--- a2/source.py
+++ a2/source.py
@@ -56,10 +56,6 @@ def q1():
 # End of Q1 -------------------------------------------------------------------
 
 
-# TODO remove bc not in use
-# def f(X, w, w0):
-#     return 1 / 1 + np.exp( -(np.matmul(w.T, X) + w0)  )
-
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
@@ -68,6 +64,46 @@ def probability(x, w, w0):
 
 def predict(x, w, w0, threshold=0.5):
     return probability(x, w, w0) >= threshold
+
+def get_metrics(X, t, N0, N1, logReg, threshold=0.5):
+
+    # P(C = 1|x) = threshold
+    # calculate 1500 x 1 class prediction
+    z = predict(X, logReg.coef_, logReg.intercept_, threshold=threshold)
+    z = z.reshape(z.size)
+    col_z = z[:, np.newaxis] # turn into column vector
+    # print 'z'
+    # print z
+    # print z.shape
+
+    col_t = t[:, np.newaxis] # turn into column vector
+    # print 't'
+    # print t
+    # print t.shape
+
+    table = np.concatenate((col_z, col_t), axis=1) # each row contains [prediction, target]
+    # print 'table'
+    # print table
+
+    # test and counts number of rows that have [1, 1] which is a True Positive
+    TP = np.count_nonzero(np.all(table, axis=1))
+
+    # test and counts number of rows that have [1, 0] which is a False Positive
+    FP_table = np.zeros(table.shape)
+    FP_table[:, 0] = 1
+    FP = np.count_nonzero(np.all(np.equal(table, FP_table), axis=1))
+
+    # test and counts number of rows that have [0, 1] which is a False Negative
+    # TODO replace TP + FN with N1
+    # FN_table = np.zeros(table.shape)
+    # FN_table[:, 1] = 1
+    # FN = np.count_nonzero(np.all(np.equal(table, FN_table), axis=1))
+
+    print '\nP(C = 1|x) = ' + str(threshold)
+    print '\tprecision'
+    print '\t\t' + str(TP / (float(TP) + FP))
+    print '\trecall P(C = 1|x) = ' + str(threshold)
+    print '\t\t' + str(TP / (float(N1)))
 
 def q2():
     # Question 2(a)
@@ -108,30 +144,11 @@ def q2():
     # second way - for w and w_0
     # which is correct predictions / number of predictions
 
-
     # calculate 1500 x 1 class prediction
-    # print 'X'
-    # print X
-    # print 'X.shape'
-    # print X.shape
-    # print 'logReg.coef_.T'
-    # print logReg.coef_.T
-    # print 'logReg.coef_.T.shape'
-    # print logReg.coef_.T.shape
     z = predict(X, logReg.coef_, logReg.intercept_, threshold=0.5)
 
     # compare with target vector t
-    # print 'z'
-    # print z
-    # print 'compare Z to t'
-    # print z.shape
-    # print t.shape
-    res = np.equal(z.reshape(z.size), t)
-    # print res
-    # print res.mean()
-    # print res.shape
-
-    accuracy2 = res.mean()
+    accuracy2 = np.equal(z.reshape(z.size), t).mean()
     print '\naccuracy2:'; print '\t' + str(accuracy2)
 
     # difference of accuracies, should be zero
@@ -155,23 +172,71 @@ def q2():
     plt.xlim(-3, 6); plt.ylim(-3, 6); plt.title('Question 2(d): training data and decision boundary')
     plt.show() # TODO uncomment
 
+
     # Question 2(e)
     # Generate a scatter plot of the training data
+    plt.scatter(X[:, 0], X[:, 1], color=classToColor[t], s=2)
  
     # draw three probability contours on top of the data: 
+
+    # CS = plt.contour(xx, yy, Z, levels=[-3], colors='r', linestyles='solid')
+    # plt.clabel(CS, fmt = '%2.1d', colors = 'k', fontsize=14) #contour line labels
+
+
+    # z = predict(X, logReg.coef_, logReg.intercept_, threshold=0.5)
+    # plt.contour(X[:, 0], X[:, 1], z)
+
     # P(C = 1|x) = 0.6
+    # TODO write proof for solving sigmoid = 0.6 to find z=0.405465
+    # TODO initial whiteboard attempt to prove might have wrong sign
+    plt.plot(np.linspace(-3, 6), -(w[0] * np.linspace(-3, 6) + logReg.intercept_[0] - 0.405465) / w[1], color='b')
+
     # P(C = 1|x) = 0.5
+    # TODO write proof for solving sigmoid = 0.5 to find z=0.0
+    plt.plot(np.linspace(-3, 6), -(w[0] * np.linspace(-3, 6) + logReg.intercept_[0] - 0.0) / w[1], color='k')
+    
     # P(C = 1|x) = 0.05
+    # TODO write proof for solving sigmoid = 0.05 to find z=-2.94444
+    plt.plot(np.linspace(-3, 6), -(w[0] * np.linspace(-3, 6) + logReg.intercept_[0] + 2.94444) / w[1], color='r')
+
     plt.xlim(-3, 6); plt.ylim(-3, 6); plt.title('Question 2(e): three contours')
     plt.show()
 
 
+    # Question 2(f)
+    # Use gen data to generate test data with 10,000 points in class 0 and
+    # 5,000 points in class 1
+    N0, N1 = 10000, 5000
+    mu0, mu1 = (1, 1), (2, 2)
+    cov0, cov1 = 0, -0.9
+    X, t = gen_data(mu0, mu1, cov0, cov1, N0, N1)
 
+
+    # Question 2(g)
+    # Compute the precision and recall for each of the three contours in part (e)
+
+    # P(C = 1|x) = 0.6
+    get_metrics(X, t, N0, N1, logReg, threshold=0.6)
+
+    # P(C = 1|x) = 0.5
+    get_metrics(X, t, N0, N1, logReg, threshold=0.5)
+
+    # P(C = 1|x) = 0.05
+    get_metrics(X, t, N0, N1, logReg, threshold=0.05)
+
+# End of Q2 -------------------------------------------------------------------
+
+
+def q3():
+
+
+# End of Q3 -------------------------------------------------------------------
 
 
 # ------------------- Script for running the source file ---------------------\
 # q1()
-q2()
+# q2()
+q3()
 
 # print '\n\nQuestion 4'
 # print '----------'
